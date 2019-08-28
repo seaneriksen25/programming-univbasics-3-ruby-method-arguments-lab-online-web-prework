f9220a06bad6067cc25684e248683bbb9d846d64
Sean Eriksen <eriksensean@yahoo.com>
2019-08-28 16:24:38 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,19 +2,17 @@ def introduction (name)
   puts "Hi, my name is #{name}."
 end
 
-introduction("Sean")
 
 def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language("Sean", "Ruby")
 
 def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language_optional("Sean")
+
 
 
 
